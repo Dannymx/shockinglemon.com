a7d60051ed87082b658e3d7609fc7012a039d891
Danny Saldana <hello@dan.mx>
2022-06-01 17:25:41 -0500

    feat: add basic template with image

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,7 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'ShockingLemon.com',
+  title: 'Shocking Lemon',
+  description:
+    'Fan dedicated website for the Japanese rock band Shocking Lemon',
   locale: 'en',
 };
