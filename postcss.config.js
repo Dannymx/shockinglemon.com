ea601c64df65c77aa0f6132490a54faed306d71c
Danny Saldana <hello@dan.mx>
2022-06-01 20:33:42 -0500

    feat: add discord link and postcss nesting

--- postcss.config.js
+++ postcss.config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'tailwindcss/nesting': 'postcss-nesting',
     ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
   },
 };
