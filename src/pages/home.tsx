4bb5d963fb2cf04fa417cf2a0670e95ccfe1fd32
Danny Saldana <hello@dan.mx>
2022-07-10 00:03:33 -0500

    feat: add aspect ratios for band member swatches

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,54 @@
+import Image from 'next/image';
+
 import HomeLayout from '@/layouts/HomeLayout';
 
 const Home = () => (
   <HomeLayout>
-    <div>
-      <span className="font-bebas text-4xl font-extrabold">Welcome!</span>
-      <br />
-      <span className="font-noto text-6xl font-bold">いらっしゃいませ！</span>
-    </div>
+    <>
+      <div className="flex h-full">
+        <div className="">
+          <span className="font-bebas text-4xl font-extrabold">Welcome!</span>
+        </div>
+        <div className="relative grid aspect-member-grid h-[500px] grid-cols-2 gap-1">
+          <div className="relative ">
+            <Image
+              className="rounded-tl-3xl rounded-br-3xl"
+              src={'/assets/images/1.jpg'}
+              layout="fill"
+              objectFit="cover"
+              alt=""
+            />
+          </div>
+          <div className="relative">
+            <Image
+              className="rounded-tr-3xl rounded-bl-3xl"
+              src={'/assets/images/2.jpg'}
+              layout="fill"
+              objectFit="cover"
+              alt=""
+            />
+          </div>
+          <div className="relative">
+            <Image
+              className="rounded-tr-3xl rounded-bl-3xl"
+              src={'/assets/images/3.jpg'}
+              layout="fill"
+              objectFit="cover"
+              alt=""
+            />
+          </div>
+          <div className="relative">
+            <Image
+              className="rounded-tl-3xl rounded-br-3xl"
+              src={'/assets/images/4.jpg'}
+              layout="fill"
+              objectFit="cover"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </>
   </HomeLayout>
 );
 
