23604099900f03e1541c54479429312ae1167255
Danny Saldana <hello@dan.mx>
2022-07-11 18:50:06 -0500

    feat: add a different image

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -12,7 +12,7 @@ const HomeLayout = ({ children }: HomeProps) => (
     <div className="relative mx-auto flex h-full flex-col md:container md:pt-10 xl:max-w-7xl">
       {/* <span className={styles['background-text']}>ショッキング・レモン</span> */}
       <Header />
-      <main className="grow py-5">{children}</main>
+      <main className="grow py-10">{children}</main>
       <Footer />
     </div>
   </>
