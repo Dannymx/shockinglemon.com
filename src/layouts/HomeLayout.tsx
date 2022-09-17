34c6d1295c5a51e8cc35af8874739d6748beffe8
Danny Saldana <hello@dan.mx>
2022-09-16 22:51:01 -0500

    feat(layouts): add getLayout to the App component for others components to use

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -3,17 +3,21 @@ import type { ReactNode } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-type HomeProps = {
+type HomeLayoutProps = {
+  meta: ReactNode;
   children: ReactNode;
 };
 
-const HomeLayout = ({ children }: HomeProps) => (
-  <div className="relative mx-auto flex h-full flex-col md:container md:pt-10 xl:max-w-7xl">
-    {/* <span className={styles['background-text']}>ショッキング・レモン</span> */}
-    <Header />
-    <main className="grow py-10">{children}</main>
-    <Footer />
-  </div>
+const HomeLayout = ({ meta, children }: HomeLayoutProps) => (
+  <>
+    {meta}
+    <div className="relative mx-auto flex h-full flex-col md:container md:pt-10 xl:max-w-7xl">
+      {/* <span className={styles['background-text']}>ショッキング・レモン</span> */}
+      <Header />
+      <main className="grow py-10">{children}</main>
+      <Footer />
+    </div>
+  </>
 );
 
 export default HomeLayout;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import type { ReactElement } from "react";
 
 import {
   HomeCard,
@@ -7,67 +8,81 @@ import {
   HomeCardTitle,
 } from "@/components/Card/HomeCard";
 import HomeLayout from "@/layouts/HomeLayout";
+import { Meta } from "@/layouts/Meta";
 
-const Home = () => (
-  <HomeLayout>
-    <>
-      <div className="flex items-center justify-center">
-        <div className="z-10 -mr-28 flex w-1/3 flex-col items-center justify-center rounded-md bg-white/75 p-10 shadow-xl backdrop-blur-sm">
-          <div className="mb-2 block w-full text-2xl font-bold">
-            <span className="font-bebas">Welcome /</span>
-            <span className="font-noto">ようこそ</span>
-          </div>
+import type { NextPageWithLayout } from "./_app";
+
+const Home: NextPageWithLayout = () => (
+  <>
+    <div className="flex items-center justify-center">
+      <div className="z-10 -mr-28 flex w-1/3 flex-col items-center justify-center rounded-md bg-white/75 p-10 shadow-xl backdrop-blur-sm">
+        <div className="mb-2 block w-full text-2xl font-bold">
+          <span className="font-bebas">Welcome /</span>
+          <span className="font-noto">ようこそ</span>
+        </div>
+        <p>
+          This website is dedicated to the Japanese rock band Shocking Lemon.
+          After many years since the band disolved, many fans wanted to know
+          more about their music, so as fans of the band we decided to create
+          this website and archive their music, photos, lyrics and other rare
+          media so others fans can enjoy it and make sure Shocking Lemon is not
+          forgotten.
+        </p>
+      </div>
+      <div className="relative h-[420px] w-[700px] overflow-hidden rounded-xl shadow-2xl">
+        <Image
+          src="/assets/images/intro-band.jpg"
+          layout="fill"
+          objectFit="cover"
+          alt=""
+        />
+      </div>
+    </div>
+    <div className="flex justify-center gap-6 pt-16">
+      <HomeCard>
+        <HomeCardImage imgSrc="/assets/images/home/card1.jpg" />
+        <HomeCardBody>
+          <HomeCardTitle>Join us on Discord</HomeCardTitle>
           <p>
-            This website is dedicated to the Japanese rock band Shocking Lemon.
-            After many years since the band disolved, many fans wanted to know
-            more about their music, so as fans of the band we decided to create
-            this website and archive their music, photos, lyrics and other rare
-            media so others fans can enjoy it and make sure Shocking Lemon is
-            not forgotten.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
           </p>
-        </div>
-        <div className="relative h-[420px] w-[700px] overflow-hidden rounded-xl shadow-2xl">
-          <Image
-            src="/assets/images/intro-band.jpg"
-            layout="fill"
-            objectFit="cover"
-            alt=""
-          />
-        </div>
-      </div>
-      <div className="flex justify-center gap-6 pt-16">
-        <HomeCard>
-          <HomeCardImage imgSrc="/assets/images/home/card1.jpg" />
-          <HomeCardBody>
-            <HomeCardTitle>Join us on Discord</HomeCardTitle>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </p>
-          </HomeCardBody>
-        </HomeCard>
-        <HomeCard>
-          <HomeCardImage imgSrc="/assets/images/home/card2.jpg" />
-          <HomeCardBody>
-            <HomeCardTitle>Join us on Discord</HomeCardTitle>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </p>
-          </HomeCardBody>
-        </HomeCard>
-        <HomeCard>
-          <HomeCardImage imgSrc="/assets/images/home/card3.jpg" />
-          <HomeCardBody>
-            <HomeCardTitle>Join us on Discord</HomeCardTitle>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </p>
-          </HomeCardBody>
-        </HomeCard>
-      </div>
-    </>
+        </HomeCardBody>
+      </HomeCard>
+      <HomeCard>
+        <HomeCardImage imgSrc="/assets/images/home/card2.jpg" />
+        <HomeCardBody>
+          <HomeCardTitle>Join us on Discord</HomeCardTitle>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+        </HomeCardBody>
+      </HomeCard>
+      <HomeCard>
+        <HomeCardImage imgSrc="/assets/images/home/card3.jpg" />
+        <HomeCardBody>
+          <HomeCardTitle>Join us on Discord</HomeCardTitle>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua
+          </p>
+        </HomeCardBody>
+      </HomeCard>
+    </div>
+  </>
+);
+
+Home.getLayout = (page: ReactElement) => (
+  <HomeLayout
+    meta={
+      <Meta
+        title="Shocking Lemon"
+        description="Fan dedicated website for the Japanese rock band Shocking Lemon"
+      />
+    }
+  >
+    {page}
   </HomeLayout>
 );
 
