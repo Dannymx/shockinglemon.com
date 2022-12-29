85f92ddae6d2571cfeade5d29361a9ff79cf2ca5
Danny Saldana <hello@dan.mx>
2022-12-28 18:17:38 -0600

    refactor(homepage): adjust cards styles

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,5 @@
 const Footer = () => (
-  <div className="py-5 text-center text-xs">
+  <div className="py-5 px-8 text-center text-xs">
     <p>
       🍋 Shocking Lemon. All rights reserved to their respective copyright
       holders.
