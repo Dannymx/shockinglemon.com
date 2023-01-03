e11a7ae3eedb91a835fe2db157f97a6c22f6e735
Danny Saldana <hello@dan.mx>
2023-01-03 16:17:05 -0600

    feat(music): add albums with name in japanese, fix box shadows of covers

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,15 +1,17 @@
 import music from "json/music.json";
 import Cover from "components/Music/Cover";
-import type { Record } from "./types";
+import type { Record } from "components/Music/types";
 
 const Page = () => (
   <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-    {music.records.map((album: Record) => (
-      <div key={album.slug}>
-        <Cover album={album} />
-        <h4 className="pt-2 text-center">{album.name}</h4>
-      </div>
-    ))}
+    {music.records
+      .sort((a, b) => (a.release < b.release ? 1 : -1))
+      .map((album: Record) => (
+        <div key={album.slug}>
+          <Cover album={album} />
+          <h4 className="pt-2 text-center">{album.name.en}</h4>
+        </div>
+      ))}
   </div>
 );
 
