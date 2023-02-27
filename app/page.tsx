5fea5ea1aca9109c092f6428e02d90f0027b37d5
Danny Saldana <hello@dan.mx>
2023-02-27 16:10:42 -0600

    chore: remove home cards file

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,34 @@ import {
   HomeCardTitle,
 } from "components/Card/HomeCard";
 import HomeHero from "components/Hero/Home";
-import { cards } from "./home";
+
+const cards = [
+  {
+    title: "Card 1",
+    img: "/assets/images/home/card1.jpg",
+    content: (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+    ),
+  },
+  {
+    title: "Card 2",
+    img: "/assets/images/home/card2.jpg",
+    content: <p>Short Card</p>,
+  },
+  {
+    title: "Card 3",
+    img: "/assets/images/home/card3.jpg",
+    content: (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </p>
+    ),
+  },
+];
 
 export default function Page() {
   return (
