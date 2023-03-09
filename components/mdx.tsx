c440258c45250bb365acb45eb1b5ac725cfca4a1
Danny Saldana <hello@dan.mx>
2023-03-08 19:36:55 -0600

    feat: add final touches to blog post layout

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -1,7 +1,19 @@
+import type { MDXComponents } from "mdx/types";
 import { useMDXComponent } from "next-contentlayer/hooks";
+
+import { cn } from "@/lib/utils";
 
 type MdxProps = {
   code: string;
+};
+
+const components: MDXComponents = {
+  p: ({ className, ...props }) => (
+    <p
+      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
+      {...props}
+    />
+  ),
 };
 
 const Mdx = ({ code }: MdxProps) => {
@@ -9,7 +21,7 @@ const Mdx = ({ code }: MdxProps) => {
 
   return (
     <div>
-      <Component />
+      <Component components={components} />
     </div>
   );
 };
